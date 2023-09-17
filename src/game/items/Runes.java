776677d2887eb8f4cc46cef1e89a594dec50e13e
alvinandreean <alvinandreean@gmail.com>
2023-09-18 04:39:46 +0800

    REQ 2: Updated Runes and ConsumeRunesAction class.
    Modified menuDescription to show the correct description for the player,
    
    Modified execute to correctly consume the runes.
    
    Added the correct ConsumeRunesAction in the allowableActions method of Runes Class

--- src/game/items/Runes.java
+++ src/game/items/Runes.java
@@ -4,6 +4,7 @@ import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
 import game.actions.ConsumeAction;
+import game.actions.ConsumeRunesAction;
 
 public class Runes extends Item implements Consumable {
     private static final String ATTRIBUTE = "balance";
@@ -17,13 +18,14 @@ public class Runes extends Item implements Consumable {
     @Override
     public ActionList allowableActions(Actor owner) {
         ActionList actionList = new ActionList();
-        actionList.add(new ConsumeAction(this));
+        actionList.add(new ConsumeRunesAction(this));
         return actionList;
     }
 
     @Override
     public int consume(Actor actor) {
         actor.addBalance(value);
+        actor.removeItemFromInventory(this);
         return this.value;
     }
 
