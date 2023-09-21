671708cff2ed606186a42f4e256978712896b61e
Zhan Hong <zsoo0012@student.monash.edu>
2023-09-22 00:48:12 +0800

    Add EnemyFactory that acts as a factory to spawn enemy. Add a new abstract class called SpawnGround which acts as an abstract parent class for those ground that will spawn enemy. This class will contain an EnemyFactory in it so that similar ground type may spawn different enemy. Those grounds are added into each map using setGround method.

---
+++ src/game/actors/spawners/EnemyFactory.java
@@ -0,0 +1,7 @@
+package game.actors.spawners;
+
+import game.actors.enemies.Enemy;
+
+public interface EnemyFactory {
+  Enemy spawnEnemy();
+}
---
+++ src/game/grounds/spawnlocation/SpawnGround.java
@@ -0,0 +1,62 @@
+package game.grounds.spawnlocation;
+
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.enemies.Enemy;
+import game.actors.spawners.EnemyFactory;
+import java.util.List;
+
+public abstract class SpawnGround extends Ground {
+  private EnemyFactory enemyFactory;
+
+  /**
+   * Constructor.
+   *
+   * @param displayChar character to display for this type of terrain
+   */
+  public SpawnGround(char displayChar, EnemyFactory enemyFactory) {
+    super(displayChar);
+    this.enemyFactory = enemyFactory;
+  }
+
+  /**
+   * For every turn, there is a probability to spawn a new enemy If the ground is surrounded with enemies, and
+   * there is also an enemy standing on the ground, the ground will not spawn another enemy
+   * unless there is an empty space at the surrounding.
+   *
+   * @param location The location of the ground
+   */
+  @Override
+  public void tick(Location location) {
+    Enemy enemy = enemyFactory.spawnEnemy();
+    if (enemy != null){
+      Location spawnLocation = validLocation(location);
+      if (spawnLocation != null){
+        spawnLocation.addActor(enemy);
+      }
+    }
+  }
+
+  /**
+   * Find a valid location at the surrounding of the ground. Return a location if there is an
+   * empty space, return null otherwise
+   *
+   * @param location The location of the ground
+   * @return a valid location if there is an empty space at the surrounding of the ground, return
+   * null otherwise
+   */
+  private Location validLocation(Location location) {
+    if (!location.containsAnActor()) {
+      return location;
+    } else {
+      List<Exit> exits = location.getExits();
+      for (Exit exit : exits) {
+        if (!exit.getDestination().containsAnActor()) {
+          return exit.getDestination();
+        }
+      }
+      return null;
+    }
+  }
+}
