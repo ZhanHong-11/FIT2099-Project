ae5929e028df4460d6bfd3b04ea3a58c0719c3b1
Zhan Hong <soozhanhong@gmail.com>
2023-10-01 19:07:17 +0800

    test commit setting

--- src/game/actors/enemies/Abxervyer.java
+++ src/game/actors/enemies/Abxervyer.java
@@ -5,6 +5,7 @@ import edu.monash.fit2099.engine.positions.Location;
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 import game.capabilities.Ability;
 import game.items.Runes;
+import java.util.Random;
 
 public class Abxervyer extends Enemy{
   private static final int BASE_INTRINSIC_WEAPON_DAMAGE = 80;
@@ -12,6 +13,7 @@ public class Abxervyer extends Enemy{
   private static final String BASE_WEAPON_VERB = "batters";
   public static final int BASE_HIT_POINTS = 2000;
   public static final int BASE_RUNES_DROP_AMOUNT = 5000;
+  private Random random = new Random();
 
   public Abxervyer() {
     super("Abxervyer, the Forest Watcher", 'Y', BASE_HIT_POINTS);
