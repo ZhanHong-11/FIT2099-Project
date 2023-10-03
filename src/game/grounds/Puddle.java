d34615201449cab539cb25ae92de40a42a793790
Zhan Hong <soozhanhong@gmail.com>
2023-10-03 17:03:31 +0800

    Upload REQ3 UML diagram. Modify ConsumeAction class so it can accept a String in constructor which will be used for menuDescription for the puddle. Puddle now implements consumable interface

--- src/game/grounds/Puddle.java
+++ src/game/grounds/Puddle.java
@@ -2,10 +2,13 @@ package game.grounds;
 
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
-import game.actions.DrinkWaterAction;
+import game.actions.ConsumeAction;
 import game.capabilities.Status;
+import game.items.Consumable;
 
 /**
  * A class that represents puddle.
@@ -14,7 +17,7 @@ import game.capabilities.Status;
  * Modified by:
  *
  */
-public class Puddle extends Ground {
+public class Puddle extends Ground implements Consumable {
     public Puddle() {
         super('~');
     }
@@ -23,8 +26,21 @@ public class Puddle extends Ground {
     public ActionList allowableActions(Actor actor, Location location, String direction) {
         ActionList actions = super.allowableActions(actor, location, direction);
         if (location.containsAnActor() && location.getActor().hasCapability(Status.HOSTILE_TO_ENEMY)) {
-            actions.add(new DrinkWaterAction());
+            actions.add(new ConsumeAction(this, menuDescription()));
         }
         return actions;
+    }
+
+    @Override
+    public String consume(Actor actor) {
+        int healthRecovery = 1;
+        actor.heal(healthRecovery);
+        actor.modifyAttribute(
+            BaseActorAttributes.STAMINA, ActorAttributeOperations.INCREASE, Math.round(0.01f * actor.getAttributeMaximum(BaseActorAttributes.STAMINA)));
+        return actor + " drank water from the puddle. Health Increased by 1 and Stamina Increased by 1%";
+    }
+
+    private String menuDescription() {
+        return "drinks water from the puddle";
     }
 }
