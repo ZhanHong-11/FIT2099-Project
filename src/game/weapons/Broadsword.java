b7c92562f97adfffddae0429caada160470c29e1
Zhan Hong <soozhanhong@gmail.com>
2023-10-05 04:14:08 +0800

    Add documentation and reformat the code for weapons package

--- src/game/weapons/Broadsword.java
+++ src/game/weapons/Broadsword.java
@@ -14,10 +14,12 @@ import game.skills.FocusSkill;
 /**
  * A subclass of SkillWeapon that represents a broadsword that have skill. A broadsword is a
  * powerful weapon that can slash enemies with high damage. It also has a special skill that can
- * increase its damage and hit rate for a short duration.
+ * increase its damage and hit rate for a short duration. This weapon can be bought and sold.
  *
  * @author Soo Zhan Hong
  * @see SkillWeapon
+ * @see Buyable
+ * @see Sellable
  */
 public class Broadsword extends SkillWeapon implements Buyable, Sellable {
 
@@ -25,6 +27,9 @@ public class Broadsword extends SkillWeapon implements Buyable, Sellable {
    * The default hit rate of the broadsword
    */
   private static final int BASE_HIT_RATE = 80;
+  /**
+   * The default sell price of the broadsword
+   */
   private static final int BASE_SELL_PRICE = 100;
 
   /**
@@ -35,6 +40,9 @@ public class Broadsword extends SkillWeapon implements Buyable, Sellable {
     this.setSkill(new FocusSkill());
   }
 
+  /**
+   * Resets the hit rate and damage multiplier of the broadsword to its default value.
+   */
   @Override
   public void resetWeapon() {
     this.updateHitRate(BASE_HIT_RATE);
@@ -53,12 +61,19 @@ public class Broadsword extends SkillWeapon implements Buyable, Sellable {
   @Override
   public ActionList allowableActions(Actor otherActor, Location location) {
     ActionList actions = super.allowableActions(otherActor, location);
-    if (otherActor.hasCapability(Ability.TRADING)){
+    if (otherActor.hasCapability(Ability.TRADING)) {
       actions.add(new SellAction(this));
     }
     return actions;
   }
 
+  /**
+   * Returns an ActionList that contains an ActivateSkillAction that allows an actor to activate the
+   * skill of the broadsword.
+   *
+   * @param owner the actor that owns the broadsword
+   * @return an unmodifiable list of Actions
+   */
   @Override
   public ActionList allowableActions(Actor owner) {
     ActionList actionList = super.allowableActions(owner);
@@ -66,6 +81,12 @@ public class Broadsword extends SkillWeapon implements Buyable, Sellable {
     return actionList;
   }
 
+  /**
+   * Reset the weapon when the weapon is picked up by an actor.
+   *
+   * @param actor the actor that picks up the broadsword
+   * @return a PickUpAction
+   */
   @Override
   public PickUpAction getPickUpAction(Actor actor) {
     resetWeapon();
@@ -84,18 +105,33 @@ public class Broadsword extends SkillWeapon implements Buyable, Sellable {
     getSkill().tickSkill(this);
   }
 
+  /**
+   * Buy the broadsword and add it to the inventory of the actor.
+   *
+   * @return A string describing the buying action
+   */
   @Override
   public String buy(Actor actor) {
     actor.addItemToInventory(new Broadsword());
     return actor + " had purchased a " + this;
   }
 
+  /**
+   * Sell the broadsword and remove it from the inventory of the actor.
+   *
+   * @return A string describing the selling action
+   */
   @Override
   public String sell(Actor actor) {
     actor.removeItemFromInventory(this);
     return actor + " had sold a " + this;
   }
 
+  /**
+   * Returns the sell price of the broadsword.
+   *
+   * @return the sell price of the broadsword
+   */
   @Override
   public int getSellPrice() {
     return BASE_SELL_PRICE;
--- src/game/weapons/GreatKnife.java
+++ src/game/weapons/GreatKnife.java
@@ -12,43 +12,80 @@ import game.items.Sellable;
 import game.skills.StabStepSkill;
 import java.util.Random;
 
+/**
+ * A subclass of SkillWeapon that represents a great knife that have skill. A great knife is a
+ * powerful weapon that can stab enemies with high damage. It also has a special skill that can
+ * attack a nearby enemy and move to a random location. This weapon can be bought and sold.
+ *
+ * @see SkillWeapon
+ * @see Buyable
+ * @see Sellable
+ */
 public class GreatKnife extends SkillWeapon implements Buyable, Sellable {
+
+  /**
+   * The default sell price of the great knife
+   */
   private static final int BASE_SELL_PRICE = 175;
   private Random random = new Random();
 
+  /**
+   * Constructs a new great knife with the default attributes and skill.
+   */
   public GreatKnife() {
     super("Great Knife", '>', 75, "stab", 70);
     this.setSkill(new StabStepSkill());
   }
 
+  /**
+   * Returns an ActionList that contains an ActivateSkillAction that allows an actor to attack
+   * another actor with the great knife's skill. If the other actor has a neutral status, it will
+   * not add this action. This method also adds a SellAction if the other actor has a trading
+   * capability.
+   *
+   * @param otherActor the other actor
+   * @param location   the location of the other actor
+   * @return a list of possible actions
+   */
   @Override
   public ActionList allowableActions(Actor otherActor, Location location) {
     ActionList actions = super.allowableActions(otherActor, location);
-    if (!otherActor.hasCapability(Status.NEUTRAL)){
+    if (!otherActor.hasCapability(Status.NEUTRAL)) {
       actions.add(new ActivateSkillAction(this, getSkill(), otherActor, location.toString()));
     }
-    if (otherActor.hasCapability(Ability.TRADING)){
+    if (otherActor.hasCapability(Ability.TRADING)) {
       actions.add(new SellAction(this));
     }
     return actions;
   }
 
+  /**
+   * Buys the item and adds it to the actor's inventory.
+   *
+   * @param actor The actor who buys the item
+   * @return String describing the result of the purchase
+   */
   @Override
   public String buy(Actor actor) {
     actor.addItemToInventory(new GreatKnife());
     return actor + " had purchased the " + this;
   }
 
+  /**
+   * Sells the item and removes it from the actor's inventory.
+   *
+   * @param actor The actor who sells the item
+   * @return String describing the result of the sale
+   */
   @Override
   public String sell(Actor actor) {
     int luck = 10;
     actor.removeItemFromInventory(this);
 
-    if (random.nextInt(100) < luck){
-      if (actor.getBalance() > getSellPrice() * 2){
+    if (random.nextInt(100) < luck) {
+      if (actor.getBalance() > getSellPrice() * 2) {
         actor.deductBalance(getSellPrice() * 2);
-      }
-      else {
+      } else {
         actor.deductBalance(actor.getBalance());
       }
       return actor + " had been scammed!";
@@ -56,6 +93,11 @@ public class GreatKnife extends SkillWeapon implements Buyable, Sellable {
     return actor + " had sold a " + this;
   }
 
+  /**
+   * Returns the sell price of the great knife
+   *
+   * @return the sell price of the great knife
+   */
   @Override
   public int getSellPrice() {
     return BASE_SELL_PRICE;
