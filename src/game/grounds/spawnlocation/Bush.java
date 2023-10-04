96c7a38fecef6c84febbdffc685c253fefde0487
Zhan Hong <soozhanhong@gmail.com>
2023-10-05 03:39:40 +0800

    Add documentation and reformat the code for grounds.spawnlocation package

--- src/game/grounds/spawnlocation/Bush.java
+++ src/game/grounds/spawnlocation/Bush.java
@@ -4,17 +4,20 @@ import game.spawners.EnemyFactory;
 
 /**
  * a class which represents a Bush where it extends to SpawnGround
+ *
  * @author Soo Zhan Hong
  * @author Marco Gotama
  * @see SpawnGround
  */
 public class Bush extends SpawnGround {
 
-    /**
-     * Constructs a new Hut
-     * @param enemyFactory represents an Enemy Factory that will spawn a certain type of enemy in this type of ground
-     */
-    public Bush(EnemyFactory enemyFactory) {
-        super('m', enemyFactory);
-    }
+  /**
+   * Constructs a new Hut
+   *
+   * @param enemyFactory represents an Enemy Factory that will spawn a certain type of enemy in this
+   *                     type of ground
+   */
+  public Bush(EnemyFactory enemyFactory) {
+    super('m', enemyFactory);
+  }
 }
--- src/game/grounds/spawnlocation/Graveyard.java
+++ src/game/grounds/spawnlocation/Graveyard.java
@@ -3,14 +3,19 @@ package game.grounds.spawnlocation;
 import game.spawners.EnemyFactory;
 
 /**
- * A class that represents the graveyard, where it extends SpawnGround
- * Created by:
+ * A class that represents the graveyard, where it extends SpawnGround Created by:
  *
  * @author Soo Zhan Hong
  * @see SpawnGround
  */
 public class Graveyard extends SpawnGround {
 
+  /**
+   * Constructs a new Graveyard
+   *
+   * @param enemyFactory represents an Enemy Factory that will spawn a certain type of enemy in this
+   *                     type of ground
+   */
   public Graveyard(EnemyFactory enemyFactory) {
     super('n', enemyFactory);
   }
