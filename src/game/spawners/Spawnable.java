16a9443c1225f6beed01e6a815cb5243b72e9526
Zhan Hong <soozhanhong@gmail.com>
2023-10-05 19:40:32 +0800

    Add a enemyFactory base class. ForestKeeperFactory and RedWolfFactory now implements weathersubscriber interface and contains a Weather controller in it so that it can makes changes when weather changed

---
+++ src/game/spawners/Spawnable.java
@@ -0,0 +1,16 @@
+package game.spawners;
+
+import game.actors.enemies.Enemy;
+
+/**
+ * An interface that represents a factory that can spawn an enemy.
+ */
+public interface Spawnable {
+
+  /**
+   * Spawns an enemy.
+   *
+   * @return The enemy that is spawned
+   */
+  Enemy spawnEnemy();
+}
