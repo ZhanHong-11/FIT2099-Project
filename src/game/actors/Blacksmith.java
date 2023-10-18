842b3cbccf06eda1944b565b68fa46bee62cd053
Vihanga <vihaaanga.mihiranga@gmail.com>
2023-10-19 05:24:54 +0800

    Added new Enum STATUS "DEFEATED_ABXERVYER" which will be associated with the player after killing abxeryyer. Will be used to unlock new Monologue for Blacksmith.

--- src/game/actors/Blacksmith.java
+++ src/game/actors/Blacksmith.java
@@ -28,7 +28,8 @@ public class Blacksmith extends Actor implements Speakable {
     this.monologues.add(new Monologue(this,"I used to be an adventurer like you, but then …. Nevermind, let’s get back to smithing.", this.name));
     this.monologues.add(new Monologue(this,"It’s dangerous to go alone. Take my creation with you on your adventure!", this.name));
     this.monologues.add(new Monologue(this, "Ah, it’s you. Let’s get back to make your weapons stronger.", this.name));
-
+    this.monologues.add(new Monologue(this, "Beyond the burial ground, you’ll come across the ancient woods ruled by Abxervyer. Use my creation to slay them and proceed further!", this.name));
+    this.monologues.add(new Monologue(this, "Hey now, that’s a weapon from a foreign land that I have not seen for so long. I can upgrade it for you if you wish.", this.name));
   }
   @Override
   public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
