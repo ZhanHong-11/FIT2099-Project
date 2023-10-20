72f0788db23f092f8ed7f5882d6751a24df6afd5
alvinandreean <alvinandreean@gmail.com>
2023-10-21 03:28:46 +0800

    A3 REQ 5: Updated Application class so that the implementation can now be tested :)

--- src/game/Application.java
+++ src/game/Application.java
@@ -48,6 +48,8 @@ public class Application {
     GameMap abandonedVillage = new AbandonedVillage(groundFactory);
     world.addGameMap(abandonedVillage);
 
+    Player player = new Player("The Abstracted One", '@', 150, 200, abandonedVillage.at(29, 5));
+
     GameMap burialGround = new BurialGround(groundFactory);
     world.addGameMap(burialGround);
 
@@ -57,7 +59,6 @@ public class Application {
     GameMap abxervyerBattleMap = new AbxervyerBattleMap(groundFactory);
     world.addGameMap(abxervyerBattleMap);
 
-    // A3 : REQ 1
     GameMap overgrownSanctuary = new OvergrownSanctuary(groundFactory);
     world.addGameMap(overgrownSanctuary);
 
@@ -71,48 +72,47 @@ public class Application {
     }
 
     abandonedVillage.at(45, 11).addActor(new Blacksmith());
-    abandonedVillage.at(23, 10).addActor(new WanderingUndead());
+    abandonedVillage.at(23, 10).addActor(new WanderingUndead(player));
     abandonedVillage.at(28, 6).addItem(new Broadsword());
-    abandonedVillage.at(40, 2).setGround(new Graveyard(new WanderingUndeadFactory()));
+    abandonedVillage.at(40, 2).setGround(new Graveyard(new WanderingUndeadFactory(player)));
     abandonedVillage.at(4, 3).setGround(new LockedGate(
-        new MoveActorAction(burialGround.at(22, 7), "to the Burial Grounds!")));
+        new MoveActorAction(burialGround.at(22, 7), "to the Burial Grounds!"), player));
 
-    burialGround.at(7, 3).setGround(new Graveyard(new HollowSoldierFactory()));
+    burialGround.at(7, 3).setGround(new Graveyard(new HollowSoldierFactory(player)));
     burialGround.at(22, 6).setGround(new LockedGate(
-        new MoveActorAction(abandonedVillage.at(5, 3), "to the Abandoned Village!")));
+        new MoveActorAction(abandonedVillage.at(5, 3), "to the Abandoned Village!"), player));
     burialGround.at(0, 8).setGround(new LockedGate(
-        new MoveActorAction(ancientWoods.at(1, 7), "to the Ancient Woods!")));
+        new MoveActorAction(ancientWoods.at(1, 7), "to the Ancient Woods!"), player));
 
     Map<String, Action> abxervyerBattleMapTravelActions = new HashMap<>();
     abxervyerBattleMapTravelActions.put("AncientWoods", new MoveActorAction(ancientWoods.at(55, 0), "to the Ancient Woods!"));
     abxervyerBattleMapTravelActions.put("OvergrownSanctuary", new MoveActorAction(overgrownSanctuary.at(16, 0), "to the Overgrown Sanctuary!"));
-    LockedGate gate = new LockedGate(abxervyerBattleMapTravelActions);
-    Abxervyer abxervyer = new Abxervyer(gate);
+    LockedGate gate = new LockedGate(abxervyerBattleMapTravelActions, player);
+    Abxervyer abxervyer = new Abxervyer(gate, player);
 
     ancientWoods.at(0, 0).addActor(new Traveller());
     ancientWoods.at(7, 7).addItem(new Bloodberry());
-    ancientWoods.at(10, 1).setGround(new Bush(new RedWolfFactory(abxervyer)));
-    ancientWoods.at(31, 10).setGround(new Bush(new RedWolfFactory(abxervyer)));
-    ancientWoods.at(43, 3).setGround(new Hut(new ForestKeeperFactory(abxervyer)));
-    ancientWoods.at(6, 6).setGround(new Hut(new ForestKeeperFactory(abxervyer)));
+    ancientWoods.at(10, 1).setGround(new Bush(new RedWolfFactory(abxervyer, player)));
+    ancientWoods.at(31, 10).setGround(new Bush(new RedWolfFactory(abxervyer, player)));
+    ancientWoods.at(43, 3).setGround(new Hut(new ForestKeeperFactory(abxervyer, player)));
+    ancientWoods.at(6, 6).setGround(new Hut(new ForestKeeperFactory(abxervyer, player)));
     ancientWoods.at(0, 7).setGround(new LockedGate(
-        new MoveActorAction(burialGround.at(0, 8), "to the Burial Grounds!")));
+        new MoveActorAction(burialGround.at(0, 8), "to the Burial Grounds!"), player));
     ancientWoods.at(55, 0).setGround(
         new LockedGate(
-            new MoveActorAction(abxervyerBattleMap.at(38, 19), "to the Abxervyer Battle Room!")));
+            new MoveActorAction(abxervyerBattleMap.at(38, 19), "to the Abxervyer Battle Room!"), player));
 
-    abxervyerBattleMap.at(23, 10).addActor(abxervyer);
+    abandonedVillage.at(29, 10).addActor(abxervyer);
     abxervyerBattleMap.at(33, 19).addItem(new GiantHammer());
-    abxervyerBattleMap.at(1, 8).setGround(new Hut(new ForestKeeperFactory(abxervyer)));
-    abxervyerBattleMap.at(8, 1).setGround(new Hut(new ForestKeeperFactory(abxervyer)));
-    abxervyerBattleMap.at(33, 17).setGround(new Bush(new RedWolfFactory(abxervyer)));
+    abxervyerBattleMap.at(1, 8).setGround(new Hut(new ForestKeeperFactory(abxervyer, player)));
+    abxervyerBattleMap.at(8, 1).setGround(new Hut(new ForestKeeperFactory(abxervyer, player)));
+    abxervyerBattleMap.at(33, 17).setGround(new Bush(new RedWolfFactory(abxervyer, player)));
 
-    overgrownSanctuary.at(16, 1).setGround(new Hut(new EldentreeGuardianFactory()));
-    overgrownSanctuary.at(18, 1).setGround(new Bush(new LivingBranchFactory()));
+    overgrownSanctuary.at(16, 1).setGround(new Hut(new EldentreeGuardianFactory(player)));
+    overgrownSanctuary.at(18, 1).setGround(new Bush(new LivingBranchFactory(player)));
 
-    Player player = new Player("The Abstracted One", '@', 150, 200, abandonedVillage.at(29, 5));
+
     world.addPlayer(player, abandonedVillage.at(29, 5));
-
     world.run();
   }
 }
